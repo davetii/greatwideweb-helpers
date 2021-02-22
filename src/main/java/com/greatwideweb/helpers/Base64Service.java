b7f49c0c129b8630fc1ecace255fdbe8daf07b90
Dave Turner <davetii@gmail.com>
2021-02-21 19:10:39 -0500

    Adding Logging

--- src/main/java/com/greatwideweb/helpers/Base64Service.java
+++ src/main/java/com/greatwideweb/helpers/Base64Service.java
@@ -1,6 +1,10 @@
 package com.greatwideweb.helpers;
 
+
+
 import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.text.DateFormat;
 import java.util.Date;
 
+
+
 import static com.greatwideweb.helpers.Base64Util.BASE_64_UTIL;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
@@ -17,28 +23,26 @@ import static org.apache.commons.lang3.StringUtils.isEmpty;
 @RestController
 public class Base64Service {
 
-
-    @Value( "${app.u}" )
-    private String u;
+    Logger log = LoggerFactory.getLogger(Base64Service.class);
 
     @RequestMapping("/base64encode")
     public String encodeBase64(HttpServletRequest request, @RequestParam(value="v", defaultValue="") String v) {
         if (isEmpty(v)) { return EMPTY; }
-        System.out.println(request.getRemoteAddr());
+        log.info("request address: " + request.getRemoteAddr());
         return BASE_64_UTIL.encodeBase64(v);
     }
 
     @RequestMapping("/base64decode")
     public String decodeBase64(HttpServletRequest request, @RequestParam(value="v", defaultValue="") String v) {
         if (isEmpty(v)) { return EMPTY; }
-        System.out.println(request.getRemoteAddr());
+        log.info("request address: " + request.getRemoteAddr());
         return BASE_64_UTIL.decodeBase64(v);
     }
 
     @RequestMapping("/ping")
-    public String ping() {
-        //return DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT).format(new Date());
-        return u;
+    public String ping(HttpServletRequest request) {
+        log.info("request address: " + request.getRemoteAddr());
+        return DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT).format(new Date());
     }
 
 
